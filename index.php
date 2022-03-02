9e58646fe678f1c1c438cfa6bac144278dc8588f
Kevin Chocano <kevinchocano@gmail.com>
2022-03-02 18:24:01 -0300

    se agrego funcion ajax, la cual funciona como prueba y editarUsuario.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ $db= new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 			$db->preparar("SELECT * FROM usuarios");
 			$db->ejecutar();
 			$db->prep()->bind_result($id,$nombre_BD,$hobby_BD); 
-//$nombre_BD="Hola";
-//$hobby_BD="Adios";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,6 +49,7 @@ $db= new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 				    <tr>
 				      <th scope="col">Nombre</th>
 				      <th scope="col">Hobby</th>
+				      <th scope="col">Eliminar</th>
 				    </tr>
 			    </thead>
 			    <tbody>
@@ -57,6 +57,7 @@ $db= new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 					  	<tr>
 					      <td><?php echo $nombre_BD; ?></td>
 					      <td><?php echo $hobby_BD; ?></td>
+					      <td><button onclick="mostrarDatos()">Click me</button></td>
 					    </tr>
 					<?php } ?>
 			    </tbody>
@@ -68,5 +69,24 @@ $db= new Database(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 	<script src="js/jquery-3.5.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/bootstrap.bundle.min.js"></script>
+	<script >
+		function mostrarDatos(){
+			console.log("Probando mostrarDatos");
+			$.ajax({
+				type:"POST",
+				url:"editarRegistro.php",
+				dataType: "json",
+				data: {},
+				success:function(data){
+					if(data.status == 'ok') {
+						document.getElementById("sueldo").value = data;
+					} else 
+					{
+						document.getElementById("sueldo").value = "Nivel y Categoria deben ser seleccionados" ;
+					}
+				}
+			});
+		}
+	</script>
 </body>
 </html>
