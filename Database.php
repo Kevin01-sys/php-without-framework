3b19165c6d84a44965d22a918b667b7507bbcd8f
Kevin Chocano <kevinchocano@gmail.com>
2022-03-02 02:22:09 -0300

    Funciona el proyecto, pero falta agregarle que inserte los datos

--- Database.php
+++ Database.php
@@ -5,6 +5,7 @@ class Database{
     protected $db;//sera una instancia de la clase mysqli
     protected $prep;/*sera una instancia del objeto que devuelve el metodo "prepare" de la instancia $db de la clase mysqli*/
     protected $consulta;/*sera la consulta sql que enviaremos como parametro al metodo preparar*/
+    protected $resultado;/*se usará para validar los datos del usuario*/
 
 
     /*En el metodo constructor de la clase, $db pasara a ser una instancia de la clase mysqli, despúes verificamos que no haya habido algún error, usando la propiedad "connect_errno" que devolvera un codigo de error, si envia 0 entonces no ocurrieron errores. (0 = false).*/
@@ -15,43 +16,6 @@ class Database{
             echo "error<br>Fallo la conexion con Mysql, tipo de error -> ({$this->db->connect_error}) <a href='index.php'>Regresar</a>";  
         }
     }
-
-    /*En el metodo preparar, recibiremos la consulta como parametro y se la daremos al metodo "prepare" que la preparara; mandara false si el servidor tuvo problemas con la sentencia de consulta.*/
-
-    public function preparar($consulta){
-        $this->consulta= $consulta;
-        $this->prep= $this->db->prepare($this->consulta);
-        if(!$this->prep){
-            echo("Error al preparar la consulta. <a href='index.php'>Regresar</a>");
-        } 
-    }
-
-    /*Dado que "prepare" además de preparar la consulta devuelve un objeto tipo sentencia, es este último quien tiene el método "execute" el cual ejecuta la sentencia en el servidor.*/
-
-    public function ejecutar(){
-        //echo("Error al preparar la consulta. <a href='index.php'>Regresar</a>");
-        echo("@");
-        //$this->prep->execute();
-    }
-
-    /*Usaremos una función pública para acceder a la sentencia que es una variable encapsulada/protected desde afuera de la clase, ya que usaremos un método que posee.*/
-
-    public function prep(){
-        return $this->prep;
-    }
-
-    /*El metodo resultado hara uso del metodo fetch que trae una fila de la busquedad que haya hecho en la base de datos, dado que solo trae una, haremos uso de un ciclo while despúes, para que muestre todos los registros.*/
-
-    public function resultado(){
-        return $this->prep->fetch(); 
-    }
-
-    /*Literalmente libera de memoria la consulta, es regla, de otra manera nos mandara error si intentamos otra consulta*/
-    
-    public function liberar(){
-        echo("@");
-    }
-
     /*El método "query" es directo de la instancia $db de la clase mysqli, nos permite hacer una consulta directa, la uso en este "validadDatos" para corroborar si el usuario ya existe, enviándole de parametros, la columna que queremos, de que tabla y bajo que condición, es un SELECT sencillo.*/
 
     public function validarDatos($columna, $tabla, $condicion){
@@ -59,12 +23,8 @@ class Database{
         $chequear= $this->resultado->num_rows;
         return $chequear; 
     }
-    
-    //Al cerrar la consulta y despúes la "conexión" liberamos la memoria, es regla.
-    public function cerrar(){
-        $this->prep->close();
-        $this->db->close();
-    }
-    
+
+
+
 }
 ?>
--- registro.php
+++ registro.php
@@ -10,8 +10,7 @@
 	<?php
 
 	/*Con el require_once haremos uso del archivo Config.php donde guardamos en constantes los datos requeridos para conectarnos a la BD.*/
-
-	require_once "config.php";
+	require_once "Config.php";
 
 
 	/*La función spl_autoload_register permite cargar o llamar automáticamente una función que le pasemos, es decir es un "autoloader", dentro pasaremos un "require_once" con un parametro $clase, el truco es que, cuando instanciemos una clase que no esta en nuestro archivo, esa variable tomara el nombre de la clase, en el caso más abajo será "Database", así si en un futuro tenemos muchas clases en distintos archivos, no tendremos que hacer un "require" o un "include" por cada una.*/
@@ -22,7 +21,7 @@
 	});
 
 	/*"extract" tomara los datos del Array que vienen el POST y los convertira en variables declarandolas usando como valor los "name" que tienen cada input de nuestro formulario, es decir, de lo extraido en POST saldran las variables $nombre y $hobby con los valores que hayamos introducido en el formulario.*/
-
+	
 	extract($_POST, EXTR_OVERWRITE);
 
 	//Instanciamos la clase Database para hacer la conexión y las consultas.
@@ -35,60 +34,7 @@
 	if($validarUsuario>0){
 		echo "Usuario ya registrado, ingrese otros datos. <a href='index.php'>Regresar</a>";
 	}else{
-
-		/*Ahora si no está registrado el nombre que le pasamos entonces haremos el registro; en la función "preparar" mandaremos la inserción de datos usando las variables que previamente "extract" extrajo de lo venido en el POST.*/
-
-			$db->preparar("INSERT INTO usuarios VALUES ('$nombre','$hobby')");
-
-		/*Ahora por regla usaremos el "ejecutar" y ya se habra logrado la inserción en la BD*/
-
-			$db->ejecutar();
-			echo("Te has registrado perfectamente.");
-
-		/*Liberamos para que podamos hacer la siguiente consulta, es regla.*/
-
-			$db->liberar();
-		
-		/*Ahora en vez de insertar haremos una selección de todos los datos y ejecutaremos*/
-
-			$db->preparar("SELECT * FROM usuarios");
-			$db->ejecutar();
-
-		/*Aquí haremos uso de la consulta que estaba encapsulada en la clase, por que haremos uso del método "bind_result" que nos asigna cada registro que la consulta haya hecho a las variables que le suministremos (Se declaran dentro del mismo paso de parametros).*/
-
-			//$db->prep()->bind_result($nombre_BD,$hobby_BD);
-	
-		/*Aquí crearemos la tabla a base del uso de "echo" con código HTML*/
-
-			echo '<table class="table">
-			  <thead>
-			    <tr>
-			      <th scope="col">Nombre</th>
-			      <th scope="col">Hobby</th>
-			    </tr>
-			  </thead>
-			  <tbody>';
-
-		/*Para mostrar en fila cada resultado encontrado en la BD, haremos uso del metodo "resultado" que recordemos tiene el "return $this->prep->fetch();" el metodo "fetch" de la consulta trae una fila de la tabla resultado de la consulta, usamos un while para que itere con cada registro encontrado, de no usarlo, entonces solo nos enviaria la primera fila de todo lo encontrado*/ 
-
-			  while($db->resultado()){
-			  	echo"
-			  	<tr>
-			      <td>$nombre_BD</td>
-			      <td>$hobby_BD</td>
-			    </tr>
-			    ";
-			  } 
-			 echo '</tbody>
-			</table>';
-
-		/*Liberamos la consulta*/
-
-			$db->liberar();
-
-		/*Y finalmente cerramos toda la conexión*/
-		
-			$db->cerrar();
+		echo "Usuario ingresado con éxito";
 	}
 
 	
