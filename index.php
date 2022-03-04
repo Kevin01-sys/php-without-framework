ef49aba0f11f80c01e5fa4cc64d8770c30211f69
Kevin Chocano <kevinchocano@gmail.com>
2022-03-04 04:48:54 -0300

    Modal correcto, botones editar y borrar traen los datos a los input

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
-$id=13;
- ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Tutorial Conexión PHP BD puro</title>
 	<!-- Css usado para dar estilo a la hoja -->
@@ -13,7 +10,7 @@ $id=13;
 	<!--Librerias para el uso del Datatable-->
 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
 	<!-- Librerias para el uso de bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">-->
 	<!-- Librerias para los iconos de los botones -->
 	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 	<!-- Con el link rel y los 2 script es que se puede levantar el modal -->
@@ -29,13 +26,14 @@ $id=13;
 				<h1>Por favor ingrese los datos:</h1>
 
 				<form method="post" action="registro.php">
+				  <input type="hidden" class="form-control" id="idusuario" name="idusuario">
 				  <div class="form-group">
 				    <label for="nombre">Nombre</label>
-				    <input type="text" class="form-control" name="nombre" > 
+				    <input type="text" class="form-control" id="nombreusuario" name="nombre" > 
 				  </div>
 				  <div class="form-group">
 				    <label for="hobby">Hobby</label>
-				    <input type="text" class="form-control" name="hobby" > 
+				    <input type="text" class="form-control" id="hobby" name="hobby" > 
 				  </div>
 				  <button type="submit" class="btn btn-primary">Enviar</button>
 				  <input type="button" id="btn_listar" class="btn btn-primary" value="Listar">
@@ -69,24 +67,29 @@ $id=13;
 	</div>
 
   <!-- Modal Eliminar usuario -->
-  <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel">
-    <div class="modal-dialog" role="document">
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="modalEliminarLabel">Eliminar usuario</h4>
-        </div>
-        <div class="modal-body">
-          ¿Estás seguro de eliminar al usuario?<strong data-name=""></strong>
-        </div>
-        <div class="modal-footer">
-          <button type="button" onclick="" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
+  <form id="frmEliminarUsuario" action="" method="POST">
+  		<input type="hidden" id="idusuarioeliminar" value="">
+  		<input type="hidden" id="opcion" name="opcion" value="eliminar">
+		  <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel">
+		    <div class="modal-dialog" role="document">
+		      <!-- Modal content-->
+		      <div class="modal-content">
+		        <div class="modal-header">
+		          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		          <h4 class="modal-title" id="modalEliminarLabel">Eliminar usuario</h4>
+		        </div>
+		        <div class="modal-body">
+		          ¿Estás seguro de eliminar al usuario?<strong data-name=""></strong>
+		        </div>
+		        <div class="modal-footer">
+		          <button type="button" onclick="" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+		          <button type="button" onclick="" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+		        </div>
+		      </div>
+		      
+		    </div>
+		  </div>
+  </form>
   <!-- Fin Modal Eliminar usuario -->
 
 	<!-- Termino de div listar -->
@@ -98,6 +101,8 @@ $id=13;
           console.log("Probando document ready");
           listar();
        });
+
+
 
        //Al presionar el boton btn_listar, ocurrira la función listar()
        $("#btn_listar").on("click", function(){
@@ -117,20 +122,37 @@ $id=13;
     				{"data":"id"},
     				{"data":"nombre"},
     				{"data":"hobby"},
-    				{"defaultContent":'<button type="button" id="buttonEditar" class="editar btn btn-primary"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="eliminar btn btn-danger" data-toggle="modal" data-target="#modalEliminar"><i class="fa fa-trash-o"></i></button>'},
+    				{"defaultContent":'<button type="button" id="buttonEditar" class="editar btn btn-primary"><i class="fa fa-pencil-square-o"></i></button> <button type="button" id="buttonEliminar" class="eliminar btn btn-danger" data-toggle="modal" data-target="#modalEliminar"><i class="fa fa-trash-o"></i></button>'},
     			],
     			"language": idioma_espanol,
     		});
 
     		obtener_data_editar("#dt_cliente", table);
+
+    		obtener_id_eliminar("#dt_cliente", table);
+
+
     	}
 
     	var obtener_data_editar = function(tbody, table){
     		$(tbody).on("click", "#buttonEditar", function(){
     			var data = table.row( $(this).parents("tr")).data();
-    			console.log(data);
+    			var idusuario = $("#idusuario").val(data.id),
+    				nombre = $("#nombreusuario").val(data.nombre),
+    				hobby = $("#hobby").val(data.hobby);
+    			console.log(data);	
     		});
     	}
+
+    	var obtener_id_eliminar = function(tbody, table){
+    		$(tbody).on("click", "#buttonEliminar", function(){
+    			var data = table.row( $(this).parents("tr")).data();
+    			var idusuario = $("#frmEliminarUsuario #idusuarioeliminar").val(data.id);
+    			console.log(data);	
+    		});
+    	}
+
+
 
     	var idioma_espanol = {
 							    "aria": {
